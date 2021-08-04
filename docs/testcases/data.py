b08ad413f921176037e108f2b1dc38cd4c3c2973
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-04 16:53:24 +0200

    Update data.py

--- docs/testcases/data.py
+++ docs/testcases/data.py
@@ -1,12 +1,12 @@
 
 def login(driver):
-  driver.find_element_by_xpath("//a[@href='#/login']").click()
-  user_input_data = ["user200", "user200@hotmail.com", "Userpass1"]
-  # Bejelentkezési űrlap feltöltése
-  for i in range(len(user_input_data) - 1):
-      driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(user_input_data[i + 1])
-
-  driver.find_element_by_tag_name("button").click()
-
+  sign_in_btn = driver.find_element_by_xpath('//a[@href="#/login"]')
+  sign_in_btn.click()
+  email_input = driver.find_element_by_xpath('//input[@placeholder="Email"]')
+  password_input = driver.find_element_by_xpath('//input[@placeholder="Password"]')
+  email_input.send_keys("user200@hotmail.com")
+  password_input.send_keys("Userpass1")
+  sign_up_button = driver.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+  sign_up_button.click()
 
   
