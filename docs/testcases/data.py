eb21669847a3cd847b39a0d1d88d8b2e15e96bb0
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-04 17:48:34 +0200

    Update data.py

--- docs/testcases/data.py
+++ docs/testcases/data.py
@@ -9,6 +9,19 @@ def find_element(driver, search_type, value):
     )
     return element
 
+def registration201(driver):
+    user_input_data = ["user201", "user201@hotmail.com", "Userpass1"]
+
+    driver.find_element_by_xpath("//a[@href='#/register']").click()
+
+    # Beviteli mezők feltöltése a random user adatokkal
+    for i in range(len(user_input_data)):
+        driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(user_input_data[i])
+    driver.find_element_by_tag_name("button").click()
+    time.sleep(2)
+    # Értesítési ablak bezárása
+    driver.find_element_by_xpath("//button[normalize-space()='OK']").click()
+
 
 def login(driver):
     sign_in_btn = driver.find_element_by_xpath('//a[@href="#/login"]')
