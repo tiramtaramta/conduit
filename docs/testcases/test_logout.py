a3b51a4c9d3510fb0bf4b2d9ae0f8814b8a83132
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-04 17:39:33 +0200

    Update test_logout.py

--- docs/testcases/test_logout.py
+++ docs/testcases/test_logout.py
@@ -1,31 +1,23 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import time
-from data import login, find_element
+from data import login
 
 
 # -------- A005, TC-0003 Kijelentkezés --------
 def test_logout():
     browser_options = Options()
-    browser_options.headless = True
+    browser_options.headless = False
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
     driver.get("http://localhost:1667/#/")
-    
+
     login(driver)
-    element = find_element(driver, By.XPATH, '//a[@active-class="active"]')
-    element.click()
-    
-
-#     time.sleep(5)
-#     driver.find_element_by_xpath("//a[@active-class='active']").click()
-
-
+    time.sleep(4)
+    driver.find_element_by_xpath('//a[@active-class="active"]').click()
+    time.sleep(2)
     # Kijelentkezés tényének ellenőrzése
-#     sign_out_check = driver.find_element_by_xpath("//a[starts-with(@href, '#/login')]").text
-#     assert sign_out_check == f"{sign_out_check}", f"Test Failed: User is logged in."
+    sign_out_check = driver.find_element_by_xpath("//a[starts-with(@href, '#/login')]").text
+    assert sign_out_check == f"{sign_out_check}", f"Test Failed: User is logged in."
 
-#     time.sleep(1)
-
-      
     driver.quit()
