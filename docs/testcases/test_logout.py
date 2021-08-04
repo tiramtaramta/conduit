00fbedbd5d47c239be2b0eae1d9eeeabdd69c20d
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-04 17:49:22 +0200

    Update test_logout.py

--- docs/testcases/test_logout.py
+++ docs/testcases/test_logout.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from data import login
+from data import login, registration201
 
 
 # -------- A005, TC-0003 Kijelentkezés --------
@@ -12,12 +12,11 @@ def test_logout():
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
     driver.get("http://localhost:1667/#/")
 
-    login(driver)
+    registration201(driver)
     time.sleep(4)
     driver.find_element_by_xpath('//a[@active-class="active"]').click()
     time.sleep(2)
     # Kijelentkezés tényének ellenőrzése
     sign_out_check = driver.find_element_by_xpath("//a[starts-with(@href, '#/login')]").text
     assert sign_out_check == f"{sign_out_check}", f"Test Failed: User is logged in."
-
     driver.quit()
