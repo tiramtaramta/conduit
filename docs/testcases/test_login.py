e2fb36f0a3848b5b008f8b39f22de001cc24af70
Vaska Gergely <vaska.gergely@gmail.com>
2021-08-04 14:03:18 +0200

    login

---
+++ docs/testcases/test_login.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import random
+import time
+import csv
+
+
+# -------- A004, TC-0010 Bejelentkezés helyes adatokkal --------
+def test_login():
+    browser_options = Options()
+    browser_options.headless = True
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
+    driver.maximize_window()
+
+    try:
+        driver.get("http://localhost:1667/#/")
+
+        time.sleep(2)
+
+        def login_process():
+            driver.find_element_by_xpath("//a[@href='#/login']").click()
+
+            # Automata regisztrációval létrejött felhasználók beolvasása
+            with open('registered_users.csv') as users_file:
+                csv_reader = csv.reader(users_file, delimiter=';')
+                for row in csv_reader:
+                    users_list = row
+
+            # Random felhasználó kiválasztása a bejelentkezéshez
+            random_user_index = random.randint(0, len(users_list) - 1)
+            random_user_name = users_list[random_user_index]
+            user_input_data = [random_user_name, f"{random_user_name}@hotmail.com", "Userpass1"]
+
+            # Bejelentkezési űrlap feltöltése
+            for i in range(len(user_input_data) - 1):
+                driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(user_input_data[i + 1])
+
+            time.sleep(1)
+
+            driver.find_element_by_tag_name("button").click()
+
+            time.sleep(2)
+
+            # Bejelentkezés tényének ellenőrzése
+            username_check = driver.find_element_by_xpath("//a[starts-with(@href, '#/@')]").text
+            assert username_check == user_input_data[0], f"Test Failed: User is not logged in ({user_input_data[0]})."
+
+            time.sleep(2)
+
+        login_process()
+
+    finally:
+        driver.quit()
