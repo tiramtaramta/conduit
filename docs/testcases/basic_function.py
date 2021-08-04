bee3d9cf0d834ef4ab6002a770dbce65310a94ac
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-04 20:42:52 +0200

    Update and rename data.py to basic_function.py

--- docs/testcases/basic_function.py
+++ docs/testcases/basic_function.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -9,29 +10,31 @@ def find_element(driver, search_type, value):
     )
     return element
 
-def registration201(driver):
-    user_input_data = ["user201", "user201@hotmail.com", "Userpass1"]
+
+def basic_registration(driver):
+    user_input_data = ["user1001", "user1008@hotmail.com", "Userpass1"]
 
     driver.find_element_by_xpath("//a[@href='#/register']").click()
 
-    # Beviteli mezők feltöltése a random user adatokkal
+    # Beviteli mezők feltöltése a user adatokkal
     for i in range(len(user_input_data)):
         driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(user_input_data[i])
     driver.find_element_by_tag_name("button").click()
     time.sleep(2)
+
     # Értesítési ablak bezárása
     driver.find_element_by_xpath("//button[normalize-space()='OK']").click()
 
 
-def login(driver):
-    sign_in_btn = driver.find_element_by_xpath('//a[@href="#/login"]')
+def basic_login(driver):
+    sign_in_btn = driver.find_element_by_xpath("//a[@href='#/login']")
     sign_in_btn.click()
-    email_input = driver.find_element_by_xpath('//input[@placeholder="Email"]')
-    password_input = driver.find_element_by_xpath('//input[@placeholder="Password"]')
+
+    email_input = driver.find_element_by_xpath("//input[@placeholder='Email']")
+    password_input = driver.find_element_by_xpath("//input[@placeholder='Password']")
+
     email_input.send_keys("user200@hotmail.com")
     password_input.send_keys("Userpass1")
-    sign_up_button = driver.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+
+    sign_up_button = driver.find_element_by_xpath("//button[@class='btn btn-lg btn-primary pull-xs-right']")
     sign_up_button.click()
-
-
-  
