dfc222d5bc4c30cdf757460c6e7a53a245423777
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-04 15:18:48 +0200

    Update test_registration200.py

--- docs/testcases/test_registration200.py
+++ docs/testcases/test_registration200.py
@@ -10,47 +10,42 @@ def test_registration200():
     browser_options = Options()
     browser_options.headless = True
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
-    driver.maximize_window()
+    driver.get("http://localhost:1667/#/")
 
-    try:
-        driver.get("http://localhost:1667/#/")
+    time.sleep(2)
 
-        time.sleep(2)
+    user_input_data = ["user200", "user200@hotmail.com", "Userpass1"]
 
-        def registration_process():
-            user_input_data = ["user200", "user200@hotmail.com", "Userpass1"]
+    driver.find_element_by_xpath("//a[@href='#/register']").click()
 
-            driver.find_element_by_xpath("//a[@href='#/register']").click()
+    # Beviteli mezők feltöltése a random user adatokkal
+    for i in range(len(user_input_data)):
+        driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(user_input_data[i])
+    driver.find_element_by_tag_name("button").click()
 
-            # Beviteli mezők feltöltése a random user adatokkal
-            for i in range(len(user_input_data)):
-                driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(user_input_data[i])
-            driver.find_element_by_tag_name("button").click()
+    time.sleep(2)
 
-            time.sleep(2)
+    # Sikeres regisztrációs értesítési ablak szövegének ellenőrzése
+    assert driver.find_element_by_class_name("swal-text").text == "Your registration was successful!"
 
-            # Sikeres regisztrációs értesítési ablak szövegének ellenőrzése
-            assert driver.find_element_by_class_name("swal-text").text == "Your registration was successful!"
+    time.sleep(2)
 
-            time.sleep(2)
+    # Értesítési ablak bezárása
+    driver.find_element_by_xpath("//button[normalize-space()='OK']").click()
 
-            # Értesítési ablak bezárása
-            driver.find_element_by_xpath("//button[normalize-space()='OK']").click()
+    time.sleep(1)
 
-            time.sleep(1)
+    # Bejelentkezés tényének ellenőrzése
+    username_check = driver.find_element_by_xpath("//a[starts-with(@href, '#/@')]").text
+    assert username_check == user_input_data[
+        0], f"Test Failed: Username did not match expected ({user_input_data[0]})."
 
-            # Bejelentkezés tényének ellenőrzése
-            username_check = driver.find_element_by_xpath("//a[starts-with(@href, '#/@')]").text
-            assert username_check == user_input_data[
-                0], f"Test Failed: Username did not match expected ({user_input_data[0]})."
+    # A létrehozott felhasználó kimentése a későbbi belépésekhez
+    with open('registered_users.csv', 'a', encoding="utf-8") as csv_users:
+        csv_users.write(user_input_data[0] + ";")
 
-            # A létrehozott felhasználó kimentése a későbbi belépésekhez
-            with open('registered_users.csv', 'a', encoding="utf-8") as csv_users:
-                csv_users.write(user_input_data[0] + ";")
+    driver.quit()
 
-            time.sleep(2)
+   
 
-        registration_process()
-
-    finally:
-        driver.quit()
+   
