9bf5eb49238613e7a176d56e990efdcb58749373
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-04 20:43:18 +0200

    Update test_logout.py

--- docs/testcases/test_logout.py
+++ docs/testcases/test_logout.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from data import login, registration201, find_element
+from basic_function import basic_login, basic_registration, find_element
 
 
 # -------- A005, TC-0003 Kijelentkezés --------
@@ -13,14 +13,15 @@ def test_logout():
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
     driver.get("http://localhost:1667/#/")
 
-    registration201(driver)
+    basic_login(driver)
     time.sleep(3)
-    elem = find_element(driver, By.XPATH, '//a[@active-class="active"]')
-    elem.click()
+    driver.find_element_by_xpath("//i[@class='ion-android-exit']").click()
+    # log_out_btn = find_element(driver, By.XPATH, "//a[@active-class='active' AND class='nav-link')]")
+    # log_out_btn.click()
     time.sleep(2)
+
     # Kijelentkezés tényének ellenőrzése
     sign_out_check = driver.find_element_by_xpath("//a[starts-with(@href, '#/login')]").text
     assert sign_out_check == f"{sign_out_check}", f"Test Failed: User is logged in."
 
     driver.quit()
-
