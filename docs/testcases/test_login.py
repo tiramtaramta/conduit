4563c06e81cf20f49eb0c0b27cb170466d57a787
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-04 15:31:24 +0200

    Update test_login.py

--- docs/testcases/test_login.py
+++ docs/testcases/test_login.py
@@ -11,44 +11,35 @@ def test_login():
     browser_options = Options()
     browser_options.headless = True
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
-    driver.maximize_window()
+    driver.get("http://localhost:1667/#/")
 
-    try:
-        driver.get("http://localhost:1667/#/")
+    time.sleep(2)
 
-        time.sleep(2)
 
-        def login_process():
-            driver.find_element_by_xpath("//a[@href='#/login']").click()
+    driver.find_element_by_xpath("//a[@href='#/login']").click()
 
-            # Automata regisztrációval létrejött felhasználók beolvasása
-            with open('registered_users.csv') as users_file:
-                csv_reader = csv.reader(users_file, delimiter=';')
-                for row in csv_reader:
-                    users_list = row
+    # Automata regisztrációval létrejött felhasználók beolvasása
+    with open('registered_users.csv') as users_file:
+        csv_reader = csv.reader(users_file, delimiter=';')
+        for row in csv_reader:
+            users_list = row
 
-            # Random felhasználó kiválasztása a bejelentkezéshez
-            random_user_index = random.randint(0, len(users_list) - 1)
-            random_user_name = users_list[random_user_index]
-            user_input_data = [random_user_name, f"{random_user_name}@hotmail.com", "Userpass1"]
+    # Random felhasználó kiválasztása a bejelentkezéshez
+    random_user_index = random.randint(0, len(users_list) - 1)
+    random_user_name = users_list[random_user_index]
+    user_input_data = [random_user_name, f"{random_user_name}@hotmail.com", "Userpass1"]
 
-            # Bejelentkezési űrlap feltöltése
-            for i in range(len(user_input_data) - 1):
-                driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(user_input_data[i + 1])
+    # Bejelentkezési űrlap feltöltése
+    for i in range(len(user_input_data) - 1):
+        driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(user_input_data[i + 1])
 
-            time.sleep(1)
+    time.sleep(1)
 
-            driver.find_element_by_tag_name("button").click()
+    driver.find_element_by_tag_name("button").click()
 
-            time.sleep(2)
+    time.sleep(2)
 
-            # Bejelentkezés tényének ellenőrzése
-            username_check = driver.find_element_by_xpath("//a[starts-with(@href, '#/@')]").text
-            assert username_check == user_input_data[0], f"Test Failed: User is not logged in ({user_input_data[0]})."
-
-            time.sleep(2)
-
-        login_process()
-
-    finally:
-        driver.quit()
+    # Bejelentkezés tényének ellenőrzése
+    username_check = driver.find_element_by_xpath("//a[starts-with(@href, '#/@')]").text
+    assert username_check == user_input_data[0], f"Test Failed: User is not logged in ({user_input_data[0]})."
+    driver.quit()
