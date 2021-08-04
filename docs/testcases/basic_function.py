fc593ce14459cfb30b5b6d6cc22754282e6929cb
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-04 22:38:58 +0200

    Update basic_function.py

--- docs/testcases/basic_function.py
+++ docs/testcases/basic_function.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 
 def find_element(driver, search_type, value):
-    element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((search_type, value)))
+    element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(search_type, value))
     return element
 
 
