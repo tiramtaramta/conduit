24ad91ab2352ddb739b9f829aa5333a0ce6bb58b
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-08-06 16:51:39 +0200

    Update basic_function.py

--- docs/testcases/basic_function.py
+++ docs/testcases/basic_function.py
@@ -1,29 +1,11 @@
 import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
 
 
 def find_element(driver, search_type, value):
     element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((search_type, value)))
     return element
-
-
-def basic_registration(driver):
-    user_input_data = ["user200", "user200@hotmail.com", "Userpass1"]
-
-    driver.find_element_by_xpath("//a[@href='#/register']").click()
-
-    # Beviteli mezők feltöltése a user adatokkal
-    for i in range(len(user_input_data)):
-        driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(user_input_data[i])
-    driver.find_element_by_tag_name("button").click()
-    time.sleep(2)
-
-    # Értesítési ablak bezárása
-    ok_btn = find_element(driver, By.XPATH, "//button[normalize-space()='OK']")
-    ok_btn.click()
-    # driver.find_element_by_xpath("//button[normalize-space()='OK']").click()
 
 
 def basic_login(driver):
